111c0da77d0a334c57de9ce2868d704a68b5c460
quentin jungo <quentin.jungo@gmail.com>
2025-03-15 14:02:57 +0100

    feat: auto-center logique

--- node_2d.gd
+++ node_2d.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+const CENTER:= Vector2i(1_000, 1_000)
 const hex_ratio: float = 2/sqrt(3);
 const h: float = 40.0; # Cell size
 const overh: float = 1;
@@ -11,6 +12,32 @@ var coords: Dictionary[Vector2i, Cell.CellType] = {Vector2i(5, 5): Cell.CellType
 var cells: Array[Cell] = [Cell.new(Vector2i(5, 5), Cell.CellType.TYPE1)];
 # Coords of the tile under the mouse cursor
 var mouse_tile: Vector2i;
+
+func _ready() -> void:
+	$Camera2D.position = tile_to_pos(CENTER);
+	cells = [
+		Cell.new(Vector2i(1_005, 1_005), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_006, 1_005), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_007, 1_005), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_008, 1_005), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_009, 1_005), Cell.CellType.TYPE1),
+		#
+		Cell.new(Vector2i(1_005, 1_006), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_006, 1_006), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_007, 1_006), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_008, 1_006), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_009, 1_006), Cell.CellType.TYPE1),
+		#
+		Cell.new(Vector2i(1_005, 1_007), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_006, 1_007), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_007, 1_007), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_008, 1_007), Cell.CellType.TYPE1),
+		Cell.new(Vector2i(1_009, 1_007), Cell.CellType.TYPE1),
+	];
+	cells.shuffle()
+	coords = Utils.cells_list_to_dict(cells)
+	start_auto_move_to_center()
+
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
@@ -60,3 +87,31 @@ func pos_to_tile(pos: Vector2) -> Vector2i:
 func tile_to_pos(tile_coords: Vector2i) -> Vector2:
 	var odd = tile_coords.y % 2;
 	return h * Vector2(hex_ratio * tile_coords.x + odd * 0.5, tile_coords.y)
+
+
+
+## Lance la logique de deplacement automatique vers le centre
+## Un timer qui run toutes les x secondes
+func start_auto_move_to_center() -> void:
+	var timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 0.05
+	timer.timeout.connect(func(): cells.map(func(cell): try_to_move_to_center(cell)))
+	timer.autostart = true
+	timer.start()
+	
+## Bouge une cellule vers sa new_position
+func move_cell(cell: Cell, new_position: Vector2i) -> void:
+	coords.erase(cell.center)
+	coords[new_position] = cell.cell_type
+	cell.center = new_position
+	queue_redraw()
+
+## Bouge une cellule vers le centre si possible
+func try_to_move_to_center(cell: Cell) -> void:
+	if cell.cell_type != Cell.CellType.TYPE1:
+		return # TODO calcul different
+	var direction = Utils.vec_to_direction(CENTER - cell.center)
+	var new_position = cell.center + Utils.vec_from_dir(direction)
+	if !coords.has(new_position):
+		move_cell(cell, new_position)
