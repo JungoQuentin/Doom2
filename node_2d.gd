e4c6ee765346d06d876da66c66eb336aa5a2235c
Jungo Arnaud <arnolo.jo@gmail.com>
2025-03-15 12:46:38 +0100

    mouse_hover

--- node_2d.gd
+++ node_2d.gd
@@ -24,20 +24,36 @@ class Cell:
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		print("left click", mouse_tile)
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		mouse_tile = pos_to_tile(event.position);
+		if coords.has(mouse_tile):
+			spawn_cell(mouse_tile, CellType.TYPE1)
+			queue_redraw()
 	if event is InputEventMouseMotion:
-		mouse_tile = pos2tile(event.position);
-		print("move", event.position)
+		var new_mouse_tile = pos_to_tile(event.position);
+		if new_mouse_tile != mouse_tile:
+			mouse_tile = new_mouse_tile;
+			queue_redraw()
 
 func _draw() -> void:
+	"""
+	for i in range(10):
+		for j in range(10):
+			var pos = tile2pos(Vector2i(i, j))
+			draw_circle(pos, h/2 * overh, Color.PALE_VIOLET_RED.lightened(0.6), false, 2)
+	"""
 	for cell in cells:
-		var pos = tile2pos(cell.center)
-		draw_circle(pos, h/2 * overh, Color.LIGHT_PINK, true)
+		var pos = tile_to_pos(cell.center)
+		if cell.center == mouse_tile:
+			draw_circle(pos, h/2 * overh, Color.LIGHT_PINK.lightened(0.2), true)
+		else:
+			draw_circle(pos, h/2 * overh, Color.LIGHT_PINK, true)
 		draw_circle(pos, h/2 * overh, Color.LIGHT_PINK.darkened(0.2), false, 2)
 
 
-func spawn_cell(source_pos, cell_type):
+func spawn_cell(source_coords: Vector2i, cell_type: CellType):
+	var dir_num = randi() % 6;
+	print(dir_num)
 	# Choose 1 of 6 random directions (spawn_dir)
 	# Check is cell in 1 * spawn_dir is empty with hasmap "coords"
 	# If not, check the 5 others cells
@@ -45,12 +61,14 @@ func spawn_cell(source_pos, cell_type):
 	# ... reapete until find an empty cell
 	# add coords of all taken cells in "coords"
 	# add (center_coords, cell_type) at a random id in cells
+	
 	pass
 
-func pos2tile(pos: Vector2) -> Vector2i:
-	return Vector2i(0, 0)
+func pos_to_tile(pos: Vector2) -> Vector2i:
+	var odd = int(pos.y / h) % 2
+	return Vector2i((pos.x / h / hex_ratio - 1 - 2/3.0 - odd/3.0), (pos.y / h) - 1.9)
 
-func tile2pos(tile_coords: Vector2i) -> Vector2:
+func tile_to_pos(tile_coords: Vector2i) -> Vector2:
 	var odd = tile_coords.y % 2;
 	return h * Vector2(hex_ratio * tile_coords.x + odd * 0.5, tile_coords.y)
 	
